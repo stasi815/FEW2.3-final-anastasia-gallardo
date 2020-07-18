cc5e0b031a56f1e4e6cdc4c2b4032a0f9b4eb2c0
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-17 17:14:42 -0700

    Updates

---
+++ precache-manifest.ad51e81aadfe1c86d558f6b690f030d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c76cbb918429c8a57a16c715bd6d09",
+    "url": "/FEW2.3-final-anastasia-gallardo/index.html"
+  },
+  {
+    "revision": "bcd10de625ab9c670eb9",
+    "url": "/FEW2.3-final-anastasia-gallardo/static/css/main.517a7feb.chunk.css"
+  },
+  {
+    "revision": "37b51daf7cbd7537ed4c",
+    "url": "/FEW2.3-final-anastasia-gallardo/static/js/2.f2acd148.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FEW2.3-final-anastasia-gallardo/static/js/2.f2acd148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd10de625ab9c670eb9",
+    "url": "/FEW2.3-final-anastasia-gallardo/static/js/main.308f3972.chunk.js"
+  },
+  {
+    "revision": "cb4da683d9f0d3eaa8f5",
+    "url": "/FEW2.3-final-anastasia-gallardo/static/js/runtime-main.023cdd3f.js"
+  }
+]);
